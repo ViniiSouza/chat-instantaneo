46358d64371c33e1c248d863d525ec4c43b50e69
Vinícius <vinigsouza.bnu@gmail.com>
2023-06-21 00:20:23 -0300

    feat: implements base repository

---
+++ backend/ChatAPI/Chat/Domain/Interfaces/Repositories/IRepository.cs
@@ -0,0 +1,11 @@
+﻿namespace Chat.Domain.Interfaces.Repositories
+{
+    public interface IRepository<T>
+    {
+        public T GetById(int id);
+        public IEnumerable<T> GetAll();
+        public void Create(T entity);
+        public void Update(T entity);
+        public void Delete(T entity);
+    }
+}
---
+++ backend/ChatAPI/Chat/Infra/Repositories/Repository.cs
@@ -0,0 +1,44 @@
+﻿using Chat.Domain.Interfaces.Repositories;
+using Chat.Domain.Models;
+using Chat.Infra.Contexts;
+
+namespace Chat.Infra.Repositories
+{
+    public class Repository<T> : IRepository<T> where T : EntityBase
+    {
+        private readonly ChatDbContext _context;
+
+        public Repository(ChatDbContext context)
+        {
+            _context = context;
+        }
+
+        public void Create(T entity)
+        {
+            _context.Add(entity);
+            _context.SaveChanges();
+        }
+
+        public void Delete(T entity)
+        {
+            _context.Remove(entity);
+            _context.SaveChanges();
+        }
+
+        public IEnumerable<T> GetAll()
+        {
+            return _context.Set<T>().ToList();
+        }
+
+        public T GetById(int id)
+        {
+            return _context.Set<T>().FirstOrDefault(find => find.Id == id);
+        }
+
+        public void Update(T entity)
+        {
+            _context.Update(entity);
+            _context.SaveChanges();
+        }
+    }
+}
