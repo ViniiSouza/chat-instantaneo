5d9c55bc6508e709f94db1dfec8676576e2fe807
Vinícius <vinigsouza.bnu@gmail.com>
2023-06-22 00:37:00 -0300

    feat: implements token authentication

--- backend/ChatAPI/ChatAPI/Program.cs
+++ backend/ChatAPI/ChatAPI/Program.cs
@@ -1,38 +1,51 @@
 using AutoMapper;
-using Chat.Domain.Models;
 using Chat.Infra.Contexts;
+using Chat.Security;
 using ChatAPI.Hubs;
-using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using System.Reflection;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
+// database configuration
 builder.Services.AddDbContext<ChatDbContext>(opts =>
 {
     opts.UseSqlServer(builder.Configuration.GetConnectionString("UserConnection"));
 });
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 string profilesPath = String.Empty;
+string settingsName = String.Empty; 
 #if DEBUG
 profilesPath = @"..\Chat\bin\Debug\net6.0\Chat.dll";
+settingsName = "appsettings.Development.json";
 #else
 profilesPath = @"..\Chat\bin\Release\net6.0\Chat.dll";
+settingsName = "appsettings.json";
 #endif
-Console.WriteLine(profilesPath);
+
+// AutoMapper profiles injection
 var assembly = Assembly.LoadFrom(profilesPath);
 var profileTypes = assembly.GetTypes()
     .Where(type => typeof(Profile).IsAssignableFrom(type) && type != typeof(Profile))
     .ToArray();
 if (profileTypes != null && profileTypes.Length > 0)
     builder.Services.AddAutoMapper(profileTypes);
+
+// adds appsettings to configuration
+builder.Configuration.AddJsonFile(settingsName, optional: false, reloadOnChange: true);
+var config = builder.Configuration;
+builder.Services.AddSingleton<IConfiguration>(config);
+
+// inject configuration into token service
+TokenService.Initialize(config);
+// configure authentication
+var key = Encoding.ASCII.GetBytes(builder.Configuration.GetValue<string>("Secret"));
+builder.Services.AuthenticationConfigure(key);
 
 builder.Services.AddSignalR();
 
@@ -48,7 +61,7 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
