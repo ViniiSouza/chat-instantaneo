8171a8cdad77870f55580234f030abc498bfe090
Vinícius <vinigsouza.bnu@gmail.com>
2023-06-23 19:44:52 -0300

    feat: string extension

---
+++ backend/ChatAPI/Chat/Extensions/StringExtensions.cs
@@ -0,0 +1,28 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace Chat.Extensions
+{
+    public static class StringExtensions
+    {
+        /// <summary>
+        /// Transforms the <paramref name="str"/> into a SHA256 hash
+        /// </summary>
+        /// <param name="str"></param>
+        public static string ToShaHash(this string str)
+        {
+            StringBuilder Sb = new StringBuilder();
+
+            using (var hash = SHA256.Create())
+            {
+                Encoding enc = Encoding.UTF8;
+                byte[] result = hash.ComputeHash(enc.GetBytes(str));
+
+                foreach (byte b in result)
+                    Sb.Append(b.ToString("x2"));
+            }
+
+            return Sb.ToString();
+        }
+    }
+}
