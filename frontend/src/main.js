11ef22a719c43b3bd2ec06bf236c31128b55b6c6
Vinícius <vinigsouza.bnu@gmail.com>
2023-06-26 23:21:17 -0300

    feat: implements toast

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -4,10 +4,14 @@ import router from './router'
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css'
 
+import Toast from '@/plugins/toast.js'
+import "vue-toastification/dist/index.css"
 import './assets/main.css'
 
 const app = createApp(App)
 
 app.use(router)
+
+app.use(Toast);
 
 app.mount('#app')
