c2d563e4f4659dfcb2ab8c9757a28f09bfd6b062
Vinícius <vinigsouza.bnu@gmail.com>
2023-06-27 21:01:40 -0300

    fix: implements toast in app

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,17 +1,32 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
-import 'bootstrap';
+import Toast from 'vue-toastification'
+import 'bootstrap'
 import 'bootstrap/dist/css/bootstrap.min.css'
 
-import Toast from '@/plugins/toast.js'
-import "vue-toastification/dist/index.css"
+import 'vue-toastification/dist/index.css'
 import './assets/main.css'
 
 const app = createApp(App)
 
 app.use(router)
 
-app.use(Toast);
+const options = {
+  transition: 'Vue-Toastification__fade',
+  timeout: 2500,
+  closeOnClick: true,
+  pauseOnFocusLoss: true,
+  pauseOnHover: true,
+  draggable: true,
+  draggablePercent: 0.6,
+  showCloseButtonOnHover: false,
+  hideProgressBar: true,
+  icon: true,
+  maxToasts: 5,
+  newestOnTop: true,
+}
+
+app.use(Toast, options)
 
 app.mount('#app')
