366d05a7d48f8bb42780eb0e6522138d3da556e5
Vinícius <vinigsouza.bnu@gmail.com>
2023-07-02 18:52:50 -0300

    feat: implements tooltip component

---
+++ src/components/shared/tooltip/Tooltip.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="tooltip__container">
+    <div class="tooltip__trigger">
+      <slot name="component"> </slot>
+    </div>
+    <div class="tooltip__content">
+      <slot name="content"> </slot>
+    </div>
+  </div>
+</template>
+
+<script>
+import './shared/style.css'
+</script>
