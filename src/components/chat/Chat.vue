54019c235d7f35f38dde165fd8382986fe8fb3d7
Vinícius <vinigsouza.bnu@gmail.com>
2023-07-03 21:24:24 -0300

    feat: implements catch error in get conversation method

--- src/components/chat/Chat.vue
+++ src/components/chat/Chat.vue
@@ -8,7 +8,7 @@
           <span class="chat__conversations__header__icon">
             <font-awesome-icon icon="fa-solid fa-pen-to-square" />
           </span>
-          <span class="chat__conversations__header__icon"> 
+          <span class="chat__conversations__header__icon">
             <font-awesome-icon icon="fa-solid fa-user-plus" />
           </span>
           <span class="chat__conversations__header__icon">
@@ -19,11 +19,23 @@
         <input type="text" v-model="newMessage" placeholder="Search" />
       </div>
       <ul class="chat__conversations__list">
-        <li v-for="conversation in conversations" :key="conversation.id" class="chat__conversations__item">
-          <p class="chat__conversations__item--time">{{ getTime(conversation.lastMessage?.sendingTime) }}</p>
-          <p class="chat__conversations__item--name">{{ conversation.title }}</p>
+        <li
+          v-for="conversation in conversations"
+          :key="conversation.id"
+          class="chat__conversations__item"
+        >
+          <p class="chat__conversations__item--time">
+            {{ getTime(conversation.lastMessage?.sendingTime) }}
+          </p>
+          <p class="chat__conversations__item--name">
+            {{ conversation.title }}
+          </p>
           <span v-if="conversation.lastMessage && conversation.type == 1">
-            <font-awesome-icon v-if="conversation.lastMessage?.ownMessage" icon="fa-solid fa-check" /> {{ conversation.lastMessage?.content }}
+            <font-awesome-icon
+              v-if="conversation.lastMessage?.ownMessage"
+              icon="fa-solid fa-check"
+            />
+            {{ conversation.lastMessage?.content }}
           </span>
         </li>
       </ul>
@@ -65,27 +77,41 @@
 import './shared/style.css'
 import { ref } from 'vue'
 import api from './shared/api.js'
+import { useToast } from 'vue-toastification'
+
+const toast = useToast()
 
 const conversations = ref([])
 
-api.loadAll().then(response => {
-  if (response.status == 200) {
-    conversations.value = response.data
-  }
-})
-// INCLUIR CATCH COM TOAST DE ERRO
+api
+  .loadAll()
+  .then((response) => {
+    if (response.status == 200) {
+      conversations.value = response.data
+    }
+  })
+  .catch((err) => {
+    if (err.response && err.response.data) {
+      toast.error(err.response.data)
+    } else {
+      const errorMsg = 'Unable to load your conversations. Try again later.'
+      toast.error(errorMsg)
+    }
+  })
 
-const formatDate = date => {
+const formatDate = (date) => {
   return date.toString().padStart(2, '0')
 }
 
 const getTime = (time) => {
-  if (time == null) return ""
+  if (time == null) return ''
   const date = new Date(time)
   const today = new Date()
   if (date.toDateString() == today.toDateString()) {
     return `${formatDate(date.getHours())}:${formatDate(date.getMinutes())}`
   }
-  return `${formatDate(date.getDate())}/${formatDate(date.getMonth())}/${date.getFullYear()}`
+  return `${formatDate(date.getDate())}/${formatDate(
+    date.getMonth()
+  )}/${date.getFullYear()}`
 }
 </script>
