86a1834c4c0bd915cd6e93d81a208c7b77d444d8
Vinícius <vinigsouza.bnu@gmail.com>
2023-07-08 22:35:38 -0300

    feat: implements contact deletion

--- src/components/chat/shared/api.js
+++ src/components/chat/shared/api.js
@@ -1,15 +1,19 @@
 import axios from '@/services/axios.js'
 
-const BASE_ROUTE = 'conversation/'
+const CHAT_BASE_ROUTE = 'conversation/'
+const USER_BASE_ROUTE = 'user/'
 
 export default {
   loadAll() {
-    return axios.get(`${BASE_ROUTE}load-all`)
+    return axios.get(`${CHAT_BASE_ROUTE}load-all`)
   },
   getConversation(id) {
-    return axios.get(`${BASE_ROUTE}chat/${id}`)
+    return axios.get(`${CHAT_BASE_ROUTE}chat/${id}`)
   },
   getContacts() {
-    return axios.get(`${BASE_ROUTE}contacts`)
+    return axios.get(`${USER_BASE_ROUTE}contacts`)
+  },
+  removeContact(targetId) {
+    return axios.delete(`${USER_BASE_ROUTE}contacts/${targetId}`)
   }
 }
--- src/components/chat/shared/contacts/Contacts.vue
+++ src/components/chat/shared/contacts/Contacts.vue
@@ -1,12 +1,23 @@
 <template>
   <div class="contacts__container">
-    <input class="contacts__search" type="search" placeholder="Search for a contact name"/>
+    <input
+      class="contacts__search"
+      type="search"
+      placeholder="Search for a contact name"
+    />
     <div class="contacts__list">
-      <div class="contacts__item" v-for="contact in contacts" :key="contact.id">
+      <div class="contacts__item" v-for="(contact, index) in contacts" :key="contact.id">
         <p class="contacts__item__name">{{ contact.userName }}</p>
         <div class="contacts__item__options">
-          <font-awesome-icon class="contacts__icon contacts__icon--remove" icon="fa-regular fa-square-minus" @click="removeContact(contact)"/>
-          <font-awesome-icon class="contacts__icon contacts__icon--send" icon="fa-regular fa-paper-plane" />
+          <font-awesome-icon
+            class="contacts__icon contacts__icon--remove"
+            icon="fa-regular fa-square-minus"
+            @click="removeContact(contact, index)"
+          />
+          <font-awesome-icon
+            class="contacts__icon contacts__icon--send"
+            icon="fa-regular fa-paper-plane"
+          />
         </div>
       </div>
     </div>
@@ -41,14 +52,32 @@ api
     }
   })
 
-  const removeContact = contact => {
-    Swal.fire({
-      ...swalConfig,
-      title: `Attention`,
-      html: `Do you really want to remove <b>${contact.userName}</b> from your contacts?`,
-      showDenyButton: true,
-      confirmButtonText: 'Yes',
-      denyButtonText: 'No',
+const removeContact = (contact, index) => {
+  Swal.fire({
+    ...swalConfig,
+    title: `Attention`,
+    html: `Do you really want to remove <b>${contact.userName}</b> from your contacts?`,
+    showDenyButton: true,
+    confirmButtonText: 'Yes',
+    denyButtonText: 'No',
+  })
+    .then((result) => {
+      if (result.isConfirmed) {
+        api.removeContact(contact.id).then((payload) => {
+          if (payload.status == 200) {
+            toast.success('Succesfully deleted contact.')
+            contacts.value.splice(index, 1)
+          }
+        })
+      }
     })
-  }
+    .catch((err) => {
+      if (err.response && err.response.data) {
+        toast.error(err.response.data)
+      } else {
+        const errorMsg = 'Unable to load your contacts. Try again later.'
+        toast.error(errorMsg)
+      }
+    })
+}
 </script>
