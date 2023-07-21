a9da796fc867e65baea8400c261c5af6c87bb9bb
Vinícius <vinigsouza.bnu@gmail.com>
2023-07-20 23:22:48 -0300

    fix(mobile): deals with mobile browser's height

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,15 @@
 <script setup>
-import { RouterLink, RouterView } from 'vue-router'
+import { RouterView } from 'vue-router'
+
+// solution to deal with browser bars on mobile devices
+// from: https://medium.com/quick-code/100vh-problem-with-ios-safari-92ab23c852a8
+const appHeight = () => {
+  const doc = document.documentElement
+  doc.style.setProperty('--app-height', `${window.innerHeight}px`)
+}
+window.addEventListener('resize', appHeight)
+appHeight()
+
 </script>
 
 <template>
